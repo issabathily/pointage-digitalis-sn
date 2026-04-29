0d4e0f4610a5b934a5e339b979e91af431259ee2
issa <issabathily388@gmail.com>
2026-04-29 11:42:21 +0000

    login correction

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const baseURL = import.meta.env.VITE_API_URL || "https://pointage-digitalis-sn.onrender.com/api"
+const baseURL = import.meta.env.VITE_API_URL || "https://mon-backend-django.onrender.com/api"
 
 const api = axios.create({ baseURL })
 
--- src/services/autService.js
+++ src/services/autService.js
@@ -1,12 +1,12 @@
 import api from "./api";
 
 export const login = async (email, password) => {
-  const response = await api.post("/login", {
+  const response = await api.post("/auth/login/", {
     email: email,
     password: password
   });
 
-  const token = response.data.access_token;
+  const token = response.data.access;
   localStorage.setItem("token", token);
 
   return response;
